e58452ef1337db990c5a8769d5b98bbcf31847ef
Jon Ribbens <jribbens@users.noreply.github.com>
2002-02-18 12:22:29 +0000

    add distutils setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# $Id$
+
+from distutils.core import setup
+
+setup(name="jonpy",
+      version="0.1",
+      description="Python Web Utilities",
+      author="Jon Ribbens",
+      author_email="jon+jonpy@unequivocal.co.uk",
+      url="http://jonpy.sourceforge.net/",
+      packages=['jon']
+)
