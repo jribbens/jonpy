8fdf595bdeaedb58e9ab3dacc7aa275093475f43
jribbens <jribbens@localhost>
2002-02-25 19:54:21 +0000

    add 'new' variable to indicate if session is new add 'tidy' method add MemorySession

--- jon/session.py
+++ jon/session.py
@@ -1,7 +1,6 @@
 # $Id$
 
-import os, time, hmac, sha, Cookie, re, UserDict, random
-import jon.cgi as cgi
+import time, hmac, sha, Cookie, re, random, os, errno, fcntl
 try:
   import cPickle as pickle
 except ImportError:
@@ -12,7 +11,7 @@ class Error(Exception):
   pass
 
 
-class Session(UserDict.UserDict):
+class Session(dict):
   def make_hash(self, sid, secret):
     """Create a hash for 'sid'
     
@@ -48,11 +47,16 @@ class Session(UserDict.UserDict):
     This function may be overridden by subclasses."""
     pass
 
+  def tidy():
+    pass
+  tidy = staticmethod(tidy)
+
   def __init__(self, req, secret, cookie="jonsid", url=0, root=""):
-    UserDict.UserDict.__init__(self)
+    dict.__init__(self)
     self["id"] = None
     self.req = req
     self.relocated = 0
+    self.new = 0
 
     if self.req.cookies.has_key(cookie):
       self["id"] = req.cookies[cookie].value[:8]
@@ -78,6 +82,8 @@ class Session(UserDict.UserDict):
     if self["id"] is None:
       if self.has_key("hash"):
         del self["hash"]
+      self.created = time.time()
+      self.new = 1
       self.create(secret)
       if not self.has_key("hash"):
         self["hash"] = self.make_hash(self["id"], secret)
@@ -96,9 +102,43 @@ class Session(UserDict.UserDict):
       self.surl = root + "/" + self["id"] + self["hash"] + "/"
 
 
+class MemorySession(Session):
+  _sessions = {}
+
+  def create(self, secret):
+    while 1:
+      Session.create(self, secret)
+      if self["id"] in self._sessions:
+        continue
+      self._sessions[self["id"]] = {"created": self.created,
+        "updated": self.created, "data": {}}
+      break
+  
+  def load(self):
+    try:
+      sess = self._sessions[self["id"]]
+    except KeyError:
+      return 0
+    self.created = sess["created"]
+    self.update(sess["data"])
+    return 1
+
+  def save(self):
+    sess = self._sessions[self["id"]]
+    sess["updated"] = time.time()
+    sess["data"] = self.copy()
+
+  def tidy(cls, max_idle=0, max_age=0):
+    now = time.time()
+    for k in cls._sessions:
+      if (max_age and k["created"] < now - max_age) or \
+        (max_idle and k["updated"] < now - max_idle):
+        del cls._sessions[k]
+  tidy = classmethod(tidy)
+
+
 class FileSession(Session):
   def create(self, secret):
-    import os, errno
     while 1:
       Session.create(self, secret)
       try:
@@ -107,16 +147,19 @@ class FileSession(Session):
         if x[0] == errno.ENOENT:
           os.mkdir("%s/%s" % (self.basedir, self["id"][:2]), 0700)
       try:
-        os.open("%s/%s/%s" % (self.basedir, self["id"][:2], self["id"][2:]),
-          os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0700)
+        fd = os.open("%s/%s/%s" % (self.basedir, self["id"][:2],
+          self["id"][2:]), os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0700)
       except OSError, x:
         if x[0] != errno.EEXIST:
           raise
+        continue
+      f = os.fdopen(fd, "w")
+      f.write("%d\n" % self.created)
+      pickle.dump({}, f, 1)
+      f.flush()
       break
-    self["hash"] = self.make_hash(self["id"], secret)
 
   def load(self):
-    import fcntl, errno
     try:
       f = open("%s/%s/%s" % (self.basedir, self["id"][:2], self["id"][2:]),
         "r+")
@@ -125,34 +168,51 @@ class FileSession(Session):
         raise
       return 0
     fcntl.lockf(f.fileno(), fcntl.LOCK_EX)
-    data = f.read()
-    if data:
-      self.data.update(pickle.loads(data))
+    self.created = int(f.readline().strip())
+    self.update(pickle.load(f))
     return 1
 
   def save(self):
-    import fcntl
     f = open("%s/%s/%s" % (self.basedir, self["id"][:2], self["id"][2:]), "r+")
     fcntl.lockf(f.fileno(), fcntl.LOCK_EX)
-    pickle.dump(self.data, f, 1)
+    f.write("%d\n" % self.created)
+    pickle.dump(self.copy(), f, 1)
     f.flush()
     f.truncate()
     
-  def __init__(self, req, secret, basedir=None, **kwargs):
-    import os, errno
+  def tidy(cls, max_idle=0, max_age=0, basedir=None):
+    basedir = cls._find_basedir(basedir)
+    now = time.time()
+    for d in os.listdir(basedir):
+      if len(d) != 2 or not d.isalnum():
+        continue
+      for f in os.listdir("%s/%s" % (basedir, d)):
+        if len(f) != 6 or not f.isalnum():
+          continue
+        p = "%s/%s/%s" % (basedir, d, f)
+        if (max_idle and os.lstat(p).st_mtime < now - max_idle) or \
+          (max_age and int(open(p, "r").readline().strip()) < now - max_age):
+          os.remove(p)
+  tidy = classmethod(tidy)
+
+  def _find_basedir(basedir):
     if basedir is None:
       tmp = os.environ.get("TMPDIR", "/tmp")
       while tmp[-1] == "/":
         tmp = tmp[:-1]
       basedir = "%s/jon-sessions-%d" % (tmp, os.getuid())
-    self.basedir = basedir
     try:
       st = os.lstat(basedir)
       if st[4] != os.getuid():
-        raise Error, "Sessions basedir is not owned by user %d" % \ os.getuid()
+        raise Error, "Sessions basedir is not owned by user %d" % os.getuid()
     except OSError, x:
       if x[0] == errno.ENOENT:
         os.mkdir(basedir, 0700)
+    return basedir
+  _find_basedir = staticmethod(_find_basedir)
+
+  def __init__(self, req, secret, basedir=None, **kwargs):
+    self.basedir = self._find_basedir(basedir)
     Session.__init__(self, req, secret, **kwargs)
 
 
@@ -161,32 +221,47 @@ class SQLSession(Session):
     self.dbc.execute("LOCK TABLES %s WRITE" % (self.table,))
     while 1:
       Session.create(self, secret)
-      self.dbc.execute("SELECT 1 FROM %s WHERE ID=%%(ID)s" % (self.table,),
-        { "ID": long(self["id"], 16) })
+      self.dbc.execute("SELECT 1 FROM %s WHERE ID=%%s" % (self.table,),
+        (long(self["id"], 16),))
       if self.dbc.rowcount == 0:
         break
     self["hash"] = self.make_hash(self["id"], secret)
-    self.dbc.execute("INSERT INTO %s (ID,hash,firstAccess,lastAccess) VALUES " \
-      "(%%(ID)s,%%(hash)s,NOW(),NOW())" % (self.table,),
-      { "ID": long(self["id"], 16), "hash": self["hash"] })
+    self.dbc.execute("INSERT INTO %s (ID,hash,created,updated) VALUES " \
+      "(%%s,%%s,%%s,%%s)" % (self.table,),
+      (long(self["id"], 16), self["hash"], self.created, self.created))
     self.dbc.execute("UNLOCK TABLES")
 
   def load(self):
-    self.dbc.execute("SELECT data FROM %s WHERE ID=%%(ID)s" % (self.table,),
-      { "ID": long(self["id"], 16) })
+    self.dbc.execute("SELECT created,data FROM %s WHERE ID=%%s" % (self.table,),
+      (long(self["id"], 16),))
     if self.dbc.rowcount == 0:
       return 0
-    self.data.update(pickle.loads(self.dbc.fetchone()["data"]))
-    self.dbc.execute("UPDATE %s SET lastAccess=NOW() WHERE ID=%%(ID)s" %
-      (self.table,), { "ID": long(self["id"], 16) })
+    row = self.dbc.fetchone()
+    self.created = row[0]
+    self.update(pickle.loads(row[1]))
     return 1
 
   def save(self):
-    self.dbc.execute("UPDATE %s SET data=%%(data)s WHERE ID=%%(ID)s" %
-      (self.table,), { "ID": long(self["id"], 16),
-      "data": pickle.dumps(self.data, 1) })
+    self.dbc.execute("UPDATE %s SET updated=%%s,data=%%s"
+      " WHERE ID=%%s" % (self.table,), (time.time(),
+      pickle.dumps(self.copy(), 1), long(self["id"], 16)))
+
+  def tidy(dbc, table, max_idle=0, max_age=0):
+    if not max_idle and not max_age:
+      return
+    q = "0=1"
+    v = []
+    now = time.time()
+    if max_idle:
+      q += " OR updated < %s"
+      v.append(now - max_idle)
+    if max_age:
+      q += " OR created < %s"
+      v.append(now - max_age)
+    dbc.execute("DELETE FROM %s WHERE %s" % (table, q), v)
+  tidy = staticmethod(tidy)
     
-  def __init__(self, req, secret, dbc=None, table="sessions", **kwargs):
+  def __init__(self, req, secret, dbc, table="sessions", **kwargs):
     self.dbc = dbc
     self.table = table
     Session.__init__(self, req, secret, **kwargs)
