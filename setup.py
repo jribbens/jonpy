8b49d4f083056ca46c8af785b028e151dcfaa260
Jon Ribbens <jribbens@users.noreply.github.com>
2002-05-03 18:48:41 +0000

    set version=0.03

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name="jonpy",
-      version="0.02",
+      version="0.03",
       description="Python Web Utilities",
       author="Jon Ribbens",
       author_email="jon+jonpy@unequivocal.co.uk",
