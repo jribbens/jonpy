94fd85370d16ed3349e00d16918ae410afe52489
Jon Ribbens <jribbens@users.noreply.github.com>
2011-05-26 15:59:48 +0100

    set version=0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name="jonpy",
-      version="0.09",
+      version="0.10",
       description="Jon's Python modules",
       author="Jon Ribbens",
       author_email="jon+jonpy@unequivocal.co.uk",
